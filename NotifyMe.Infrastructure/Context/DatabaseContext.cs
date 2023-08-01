b90c13faf8aa84f7550ea28236442da8a10b0ce2
Rashit Nurzhanov <7119967@mail.ru>
2023-08-01 07:35:04 +0600

    Added the connection string to the MS SQL DB

--- NotifyMe.Infrastructure/Context/DatabaseContext.cs
+++ NotifyMe.Infrastructure/Context/DatabaseContext.cs
@@ -12,7 +12,11 @@ namespace NotifyMe.Infrastructure.Context
         public DbSet<User> Users { get; set; }
         public DbSet<UserGroup> UserGroups { get; set; }
         public DbSet<UserGroupUser> UserGroupUsers { get; set; }
-
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserGroupUser>()
